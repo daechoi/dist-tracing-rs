274eaef57c0d0a096a69f4f55230c29c6843143f
Dae Choi <daechoi@outlook.com>
2024-08-02 23:54:32 -0400

    Adds tracing subscriber initialization

--- dist-tracing-gateway/src/main.rs
+++ dist-tracing-gateway/src/main.rs
@@ -1,16 +1,33 @@
 mod api;
 mod backend;
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 use anyhow::{Context, Result};
 use configured::Configured;
 use serde::Deserialize;
+use tracing::error;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
+    if let Err(err) = init_logger() {
+        error!(
+            error = format!("{err:#}"),
+                backtrace=%err.backtrace(),
+                "dist-tracing-gateway exited with error"
+        );
+    }
     let config = Config::load().context("load configuration")?;
     let backend = backend::Backend::new(config.backend);
     let _ = api::run(config.api, backend)?.await;
     Ok(())
+}
+
+fn init_logger() -> Result<()> {
+    tracing_subscriber::registry()
+        .with(EnvFilter::from_default_env())
+        .with(fmt::layer().json())
+        .try_init()
+        .context("initialize tracing subscriber")
 }
 
 #[derive(Debug, Deserialize)]
